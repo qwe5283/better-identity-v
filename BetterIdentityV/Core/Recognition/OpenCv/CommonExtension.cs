0b64d84ae0af83aaeda0ec2a7135b7c432734bbe
qwe5283 <1074479225@qq.com>
2026-05-30 21:25:14 +0800

    feat: 支持在遮罩上绘制识别结果

---
+++ BetterIdentityV/Core/Recognition/OpenCv/CommonExtension.cs
@@ -0,0 +1,11 @@
+﻿using Color = System.Windows.Media.Color;
+
+namespace BetterIdentityV.Core.Recognition.OpenCv;
+
+public static class CommonExtension
+{
+    public static Color ToWindowsColor(this System.Drawing.Color color)
+    {
+        return Color.FromArgb(color.A, color.R, color.G, color.B);
+    }
+}
