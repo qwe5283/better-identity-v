79336728a60c502d668b9d54f1525f2acca845b6
qwe5283 <1074479225@qq.com>
2026-06-01 18:54:24 +0800

    feat: 完善可消耗道具枚举

---
+++ BetterIdentityV/GameTask/AutoPick/PickableItemType.cs
@@ -0,0 +1,25 @@
+﻿namespace BetterIdentityV.GameTask.AutoPick;
+
+// 可消耗道具
+public enum PickableItemType
+{
+    Controller, // 遥控器
+    Decoy, // 魔术棒
+    ElbowPads, // 护腕
+    Euphoria, // 忘忧之香
+    FlareGun, // 信号枪
+    Flashlight, // 手电筒
+    GulliverTravels, // 格列佛游记
+    Map, // 地图
+    PoseidonWatch, // 怀表
+    RugbyBall, // 橄榄球
+    Dovlin, // 多夫林
+    Syringe, // 镇静剂
+    Toolkit, // 工具箱
+}
+
+// 独占道具
+public enum ExclusiveItemType
+{
+    
+}
