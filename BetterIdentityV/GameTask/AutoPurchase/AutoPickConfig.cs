c4a5929f2c4c459182b018c8f19bc5827fadbe2a
qwe5283 <1074479225@qq.com>
2026-06-09 22:43:31 +0800

    chore: 添加自动购买基础架构

---
+++ BetterIdentityV/GameTask/AutoPurchase/AutoPickConfig.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BetterIdentityV.GameTask.AutoPurchase;
+
+public partial class AutoPickConfig : ObservableObject
+{
+    /// <summary>
+    /// 触发器是否启用
+    /// </summary>
+    [ObservableProperty] private bool _enabled = true;
+    
+}
---
+++ BetterIdentityV/GameTask/AutoPurchase/AutoPurchaseTrigger.cs
@@ -0,0 +1,19 @@
+﻿namespace BetterIdentityV.GameTask.AutoPurchase;
+
+public class AutoPurchaseTrigger : ITaskTrigger
+{
+    public string Name => "自动购买";
+    public bool IsEnabled { get; set; }
+    public int Priority => 30;
+    public bool IsExclusive => false;
+    
+    public void Init()
+    {
+        
+    }
+
+    public void OnCapture(CaptureContent content)
+    {
+        
+    }
+}
